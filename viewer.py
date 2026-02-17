da878e6c074b4f775285067894da738447dd6e11
Heejun Byeon <heejun.byeon@gmail.com>
2026-02-16 19:24:41 -0600

    google login is added to the viewer

--- viewer.py
+++ viewer.py
@@ -977,39 +977,53 @@ def parse_metadata_section(section):
 #     j = r.json()
 #     return j.get("documents", [])
 
-def list_runs():
+# def list_runs():
+#     url = f"{BASE_URL}/runs"
+#     r = requests.get(url)  # public read
+#     j = r.json()
+#     return j.get("documents", [])
+
+
+    # html = f"""
+    # <style>
+    #     .keycell {{
+    #         background-color: #f5f5f5;
+    #         font-weight: 400;
+    #         width: 15%;
+    #     }}
+    #     .valcell {{
+    #         background-color: white;
+    #         width: 18%;
+    #     }}
+    #     .meta-table {{
+    #         width: 100%;
+    #         border-collapse: collapse;
+    #     }}
+    #     .meta-table td {{
+    #         border: 1px solid #e0e0e0;
+    #         padding: 6px 8px;
+    #     }}
+    # </style>
+
+    # <table class='meta-table'>
+    #     <tbody>{rows}</tbody>
+    # </table>
+    # """
+    # return html
+
+def list_runs(id_token):
     url = f"{BASE_URL}/runs"
-    r = requests.get(url)  # public read
+    r = requests.get(
+        url,
+        headers={"Authorization": f"Bearer {id_token}"}
+    )
+
+    if r.status_code != 200:
+        st.error("Failed to fetch runs.")
+        return []
+
     j = r.json()
     return j.get("documents", [])
-
-
-    html = f"""
-    <style>
-        .keycell {{
-            background-color: #f5f5f5;
-            font-weight: 400;
-            width: 15%;
-        }}
-        .valcell {{
-            background-color: white;
-            width: 18%;
-        }}
-        .meta-table {{
-            width: 100%;
-            border-collapse: collapse;
-        }}
-        .meta-table td {{
-            border: 1px solid #e0e0e0;
-            padding: 6px 8px;
-        }}
-    </style>
-
-    <table class='meta-table'>
-        <tbody>{rows}</tbody>
-    </table>
-    """
-    return html
 
 
 
@@ -1510,98 +1524,99 @@ MEASURE_SPECIAL_KEYS = {
 
 
 
-# # ------------------------------------------------------------
-# # GOOGLE LOGIN
-# # ------------------------------------------------------------
-
-# if "viewer_user" not in st.session_state:
-
-#     client_id = st.secrets["google_oauth"]["client_id"]
-#     client_secret = st.secrets["google_oauth"]["client_secret"]
-
-#     redirect_uri = st.secrets["app"]["viewer_redirect_uri"]
-
-#     params = {
-#         "client_id": client_id,
-#         "redirect_uri": redirect_uri,
-#         "response_type": "code",
-#         "scope": "openid email profile",
-#         "access_type": "offline",
-#         "prompt": "select_account",
-#     }
-
-#     auth_url = "https://accounts.google.com/o/oauth2/v2/auth?" + urllib.parse.urlencode(params)
-
-#     query_params = st.query_params
-
-#     if "code" not in query_params:
-#         st.markdown(f"[Login with Google]({auth_url})")
-#         st.stop()
-
-#     code = query_params["code"]
-
-#     token_res = requests.post(
-#         "https://oauth2.googleapis.com/token",
-#         data={
-#             "code": code,
-#             "client_id": client_id,
-#             "client_secret": client_secret,
-#             "redirect_uri": redirect_uri,
-#             "grant_type": "authorization_code",
-#         },
-#     )
-
-#     token_json = token_res.json()
-
-#     if "id_token" not in token_json:
-#         st.write(token_json)
-#         st.stop()
-
-#     google_id_token = token_json["id_token"]
-
-#     user = firebase_sign_in_with_google(google_id_token, redirect_uri)
-
-#     email = user.get("email", "")
-#     if not email.endswith("@eeroq.com"):
-#         st.error("Unauthorized domain")
-#         st.stop()
-
-#     st.session_state["viewer_user"] = user
-#     st.session_state["viewer_login_time"] = time.time()
-
-#     st.query_params.clear()
-#     st.rerun()
-
-
-
-# # --- Auto-refresh token if expired ---
-# expires_in = int(st.session_state.viewer_user.get("expiresIn", "3600"))
-# login_time = st.session_state.get("login_time", time.time())
-
-# # Check if 55 minutes passed
-# if time.time() - login_time > 3300:  # 55 minutes
-#     refresh_token = st.session_state.viewer_user["refreshToken"]
-#     new_tokens = firebase_refresh_id_token(refresh_token)
-
-#     # Update session_state with new tokens
-#     st.session_state.viewer_user["idToken"] = new_tokens["id_token"]
-#     st.session_state.viewer_user["refreshToken"] = new_tokens["refresh_token"]
-#     st.session_state.viewer_user["expiresIn"] = new_tokens["expires_in"]
-#     st.session_state.login_time = time.time()
-
-# # Always use updated token
-# token = st.session_state.viewer_user["idToken"]
-# email = st.session_state.viewer_user["email"]
-
 # ------------------------------------------------------------
-# PUBLIC VIEWER MODE (No Login)
+# GOOGLE LOGIN
 # ------------------------------------------------------------
 
-# Firestore read is public.
-# Viewer does not require authentication.
+if "viewer_user" not in st.session_state:
 
-token = None
-email = "public"
+    client_id = st.secrets["google_oauth"]["client_id"]
+    client_secret = st.secrets["google_oauth"]["client_secret"]
+
+    redirect_uri = st.secrets["app"]["viewer_redirect_uri"]
+
+    params = {
+        "client_id": client_id,
+        "redirect_uri": redirect_uri,
+        "response_type": "code",
+        "scope": "openid email profile",
+        "access_type": "offline",
+        "prompt": "select_account",
+    }
+
+    auth_url = "https://accounts.google.com/o/oauth2/v2/auth?" + urllib.parse.urlencode(params)
+
+    query_params = st.query_params
+
+    if "code" not in query_params:
+        st.markdown(f"[Login with Google]({auth_url})")
+        st.stop()
+
+    code = query_params["code"]
+
+    token_res = requests.post(
+        "https://oauth2.googleapis.com/token",
+        data={
+            "code": code,
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "redirect_uri": redirect_uri,
+            "grant_type": "authorization_code",
+        },
+    )
+
+    token_json = token_res.json()
+
+    if "id_token" not in token_json:
+        st.write(token_json)
+        st.stop()
+
+    google_id_token = token_json["id_token"]
+
+    user = firebase_sign_in_with_google(google_id_token, redirect_uri)
+
+    email = user.get("email", "")
+    if not email.endswith("@eeroq.com"):
+        st.error("Unauthorized domain")
+        st.stop()
+
+    st.session_state["viewer_user"] = user
+    st.session_state["viewer_login_time"] = time.time()
+
+    st.query_params.clear()
+    st.rerun()
+
+
+
+# --- Auto-refresh token if expired ---
+expires_in = int(st.session_state.viewer_user.get("expiresIn", "3600"))
+login_time = st.session_state.get("login_time", time.time())
+
+# Check if 55 minutes passed
+if time.time() - login_time > 3300:  # 55 minutes
+    refresh_token = st.session_state.viewer_user["refreshToken"]
+    new_tokens = firebase_refresh_id_token(refresh_token)
+
+    # Update session_state with new tokens
+    st.session_state.viewer_user["idToken"] = new_tokens["id_token"]
+    st.session_state.viewer_user["refreshToken"] = new_tokens["refresh_token"]
+    st.session_state.viewer_user["expiresIn"] = new_tokens["expires_in"]
+    st.session_state.login_time = time.time()
+
+# Always use updated token
+token = st.session_state.viewer_user["idToken"]
+email = st.session_state.viewer_user["email"]
+
+
+# # ------------------------------------------------------------
+# # PUBLIC VIEWER MODE (No Login)
+# # ------------------------------------------------------------
+
+# # Firestore read is public.
+# # Viewer does not require authentication.
+
+# token = None
+# email = "public"
 
 
 def reset_filters():
@@ -1783,8 +1798,8 @@ with top_right:
 
 
 
-# runs = list_runs(token)
-runs = list_runs()
+runs = list_runs(token)
+# runs = list_runs()
 
 if not runs:
     st.info("No runs found.")
