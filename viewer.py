2f1048eb24f01de7bcefd0883849d3624460adb7
Heejun Byeon <heejun.byeon@gmail.com>
2026-02-17 09:46:38 -0600

    separte viewer editor login fix6

--- viewer.py
+++ viewer.py
@@ -1552,10 +1552,12 @@ if "viewer_user" not in st.session_state:
 
     if "code" not in query_params:
         # st.markdown(f"[Login with Google]({auth_url})")
-        st.markdown(
-            f"<a href='{auth_url}' target='_self'>Login with Google</a>",
-            unsafe_allow_html=True
-        )
+        # st.markdown(
+        #     f"<a href='{auth_url}' target='_self'>Login with Google</a>",
+        #     unsafe_allow_html=True
+        # )
+        st.link_button("Login with Google", auth_url)
+
         st.stop()
 
     code = query_params["code"]
