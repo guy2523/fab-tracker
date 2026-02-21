29553400a54a798e99b5e41a866fd2ba3e63b61e
Heejun Byeon <heejun.byeon@gmail.com>
2026-02-20 21:49:00 -0600

    modify viewer no login version 3

--- viewer_no_login.py
+++ viewer_no_login.py
@@ -1572,7 +1572,10 @@ def clear_viewer_state(keep_filters=True):
 # ---------------- TOP BAR ----------------
 top_left, top_right = st.columns([1.5, 1])
 
-# with top_right:
+with top_right:
+    redirect_uri = st.secrets["app"]["admin_redirect_uri"]
+    st.link_button("Editor", redirect_url)
+
     # st.markdown(
     #     '<div style="text-align: right;">'
     #     '<a href="https://fab-tracker-7uac3bt2l3sad66hgpvjw6.streamlit.app/" '
