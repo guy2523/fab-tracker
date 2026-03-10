7c414347897bc1b80d8c59886d6fca6b0b7c1d38
Heejun Byeon <heejun.byeon@gmail.com>
2026-03-09 20:36:01 -0500

    admin upgrade#5

--- admin_v01.py
+++ admin_v01.py
@@ -9,7 +9,7 @@ from services.presets import load_layer_presets_once
 from services.status_editor_v01 import handle_chip_status_change
 from services.flow_defaults_v01 import DEFAULT_FLOW
 from core.metadata import normalize_meta, ensure_kv_rows, build_package_chip_meta, get_package_chips, get_measure_fridges, build_measure_fridge_meta
-from ui.flow_editor import flow_editor, update_flow_editor
+from ui.flow_editor_v01 import flow_editor, update_flow_editor
 from ui.metadata_ui import render_metadata_ui, save_package_info_core, save_measure_info_core
 from services.drive import upload_file_via_cleanroom_api, delete_file_via_cleanroom_api
 import requests, time, json
